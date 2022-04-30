7357c130795ed3f4a57234c9bada50bb41a8d264
massmux <go@massmux.com>
2022-04-30 15:32:44 +0200

    check

--- backup-walletdb.sh
+++ backup-walletdb.sh
@@ -5,6 +5,7 @@
 while true; do
     inotifywait /mnt/bvb-volume/umbrel/lnd/data/chain/bitcoin/mainnet/wallet.db
     cp /mnt/bvb-volume/umbrel/lnd/data/chain/bitcoin/mainnet/wallet.db /mnt/backup/
+    rsync -av /mnt/bvb-volume/umbrel/app-data/lnbits/ /mnt/backup/lnbits/
 done
 
 

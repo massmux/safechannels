6e3e4b8656649cc7f05d36d2b493c2e5ab49fbc8
massmux <go@massmux.com>
2022-05-16 07:52:03 +0200

    little fixes

---
+++ update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# backup of channel.backup file
+orig="/mnt/bvb-volume/umbrel/lnd/data/chain/bitcoin/mainnet"
+dest="/mnt/backup"
+thefile="channel.backup"
+
+
+cp $orig/$thefile $dest/
+channelssum=`sha256sum $orig/$thefile |cut -f1 -d" "`
+echo $channelssum > $dest/o.$thefile.sha256
+
+
+# backups channel.db file
+orig="/mnt/bvb-volume/umbrel/lnd/data/graph/mainnet"
+dest="/mnt/backup"
+thefile="channel.db"
+
+cp $orig/$thefile $dest/
+channelsum=`sha256sum $orig/$thefile |cut -f1 -d" "`
+echo $channelsum > $dest/o.$thefile.sha256
+
+
+# backups wallet.db file
+orig="/mnt/bvb-volume/umbrel/lnd/data/chain/bitcoin/mainnet"
+dest="/mnt/backup"
+thefile="wallet.db"
+app="/mnt/bvb-volume/umbrel/app-data"
+
+
+cp $orig/$thefile /mnt/backup/
+walletsum=`sha256sum $orig/$thefile |cut -f1 -d" "`
+echo $walletsum > $dest/o.$thefile.sha256
+rsync -av $app/lnbits/ $dest/lnbits/
+
+
+
+
+

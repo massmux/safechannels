e3fbfc813489438ddee68c2a05412396b5710aee
Alessandro Riva <alex27riva@gmail.com>
2022-06-19 11:59:49 +0200

    Migration to Umbrel 0.5

--- backup-walletdb.sh
+++ backup-walletdb.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # backups wallet.db file
-orig="/mnt/bvb-volume/umbrel/lnd/data/chain/bitcoin/mainnet"
+orig="/mnt/data/umbrel/app-data/lightning/data/lnd/data/chain/bitcoin/mainnet"
 dest="/mnt/backup"
 thefile="wallet.db"
-app="/mnt/bvb-volume/umbrel/app-data"
+app="/mnt/data/umbrel/apps"
 
 
 while true; do
     inotifywait $orig/$thefile
-    cp $orig/$thefile /mnt/backup/
+    cp $orig/$thefile $dest/
     walletsum=`sha256sum $orig/$thefile |cut -f1 -d" "`
     echo $walletsum > $dest/o.$thefile.sha256
     rsync -av $app/lnbits/ $dest/lnbits/
 done
 
 
-# systemd
+# file location
 # /etc/systemd/system/backup-walletdb.service
-
-
-
